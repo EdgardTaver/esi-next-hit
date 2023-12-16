31e2f232a30d43da4d933320bf1b8b158fdbe6f5
Edgard Taver <edgard.vtt@gmail.com>
2023-12-16 11:22:13 -0300

    initial simple login setup

---
+++ main.py
@@ -0,0 +1,37 @@
+import hashlib
+import sqlite3
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def encrypt_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
+
+def authenticate_user(username, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT password FROM users WHERE username=?", (username,))
+    result = cursor.fetchone()
+
+    if result is not None:
+        stored_password = result[0]
+        if encrypt_password(password) == stored_password:
+            return True
+
+    return False
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if authenticate_user(username, password):
+        return jsonify({'message': 'Login successful'})
+    else:
+        return jsonify({'message': 'Invalid username or password'})
+
+if __name__ == '__main__':
+    app.run()
+
