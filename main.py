0e25d21be6f1c6326a16de3f31afd881d5aec1a5
Edgard Taver <edgard.vtt@gmail.com>
2023-12-18 02:01:19 -0300

    adds method to create playlist

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify, g
+from flask import Flask, jsonify, request, session, g
 
-from app.infrastructure.database import create_users_table, register_user, get_authenticated_user_id, start_users_database_connection
-from app.exceptions import EmailAlreadyRegisteredException
-from flask import session
+from app.exceptions import EmailAlreadyRegisteredException, PlaylistAlreadyExistsException
+from app.infrastructure.commands import (get_authenticated_user_id,
+                                         register_playlist, register_user)
+from app.infrastructure.database import start_users_database_connection
 
 app = Flask(__name__)
+app.secret_key = "my-next-hit-secret"
 
 @app.route('/login', methods=['POST']) # type:ignore
 def login():
@@ -14,10 +16,15 @@ def login():
     connection = start_users_database_connection()
     user_id = get_authenticated_user_id(connection, email, password)
     if user_id:
-        session['user_id'] = user_id
+        session["USER_ID"] = user_id
         return jsonify({'message': 'Login successful', 'user_id': user_id})
     else:
         return jsonify({'message': 'Invalid email or password'})
+
+@app.route('/logout', methods=['POST']) # type:ignore
+def logout():
+    session.pop("USER_ID", None)
+    return jsonify({'message': 'Logout successful'})
 
 @app.route('/register', methods=['POST']) # type:ignore
 def register():
@@ -32,19 +39,24 @@ def register():
     except EmailAlreadyRegisteredException:
         return jsonify({'message': 'Email already registered'}), 400
 
-@app.route('/playlist', methods=['POST']) # type:ignore
+@app.route('/playlist/create', methods=['POST']) # type:ignore
 def create_playlist():
-    user_id = session.get('user_id')
+    user_id = session.get("USER_ID")
 
-    
-    if user_id:
-        # Logic to create a new playlist for the logged in user
-        return jsonify({'message': 'Playlist created successfully'})
-    else:
+    if not user_id:
         return jsonify({'message': 'User not logged in'}), 401
 
+    connection = start_users_database_connection()
+    try:
+        playlist_name = request.json.get("name")
+        register_playlist(connection, playlist_name, user_id)
+        return jsonify({'message': 'Playlist created successfully'})
+    
+    except PlaylistAlreadyExistsException:
+        return jsonify({'message': 'Playlist already exists'}), 400
 
-
+    finally:
+        connection.close()
 
 if __name__ == '__main__':
     app.run()
