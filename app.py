b0baf4b0f3a8ad21bd7e3338aef78d960ae57df2
Edgard Taver <edgard.vtt@gmail.com>
2023-12-18 20:03:43 -0300

    adds function to control if should show add button

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if selected=="Descobrir":
             st.error("Nenhuma música encontrada")
         
         else:
-            list_musics(results)
+            list_musics(results, False)
 
     st.subheader("Descoberta diária")
     st.image(["playlist.jpg", "playlist.jpg", "playlist.jpg"], caption=["Playlist Diária", "Descubra: MPB", "Descubra: Rock"], width=150)
@@ -88,12 +88,7 @@ if selected=="Playlists":
         
         else:
             for music in musics:
-                col1, col2 = st.columns((1,2.75))
-                col1.image(music["image_url"], width=150)
-                col2.subheader(music["title"])
-                col2.write(music["artist"])
-                col2.write(f"Gênero: {music['genre']}")
-                col2.button(key=music["id"], label="Adicionar à playlist")
+                list_musics(musics, False)
 
     else:
         with st.form(key="create_playlist_form"):
--- app/frontend/components.py
+++ app/frontend/components.py
@@ -2,11 +2,13 @@ from typing import Any, Dict, List
 
 import streamlit as st
 
-def list_musics(musics: List[Dict[str, Any]]):
+def list_musics(musics: List[Dict[str, Any]], show_add_to_playlist: bool):
     for music in musics:
         col1, col2 = st.columns((1,2.75))
         col1.image(music["image_url"], width=150)
         col2.subheader(music["title"])
         col2.write(music["artist"])
         col2.write(f"Gênero: {music['genre']}")
-        col2.button(key=music["id"], label="Adicionar à playlist")
+
+        if show_add_to_playlist:
+            col2.button(key=music["id"], label="Adicionar à playlist")
