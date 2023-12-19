46914bb664703d9aeecacb9367e873731b9679cb
Edgard Taver <edgard.vtt@gmail.com>
2023-12-18 22:38:04 -0300

    wip

--- app/frontend/components.py
+++ app/frontend/components.py
@@ -8,24 +8,30 @@ from app.frontend.session import (SESSION_CLEAR_SEARCH_RESULTS,
                                   SESSION_SHOULD_EXPLORE_PLAYLIST, SESSION_USER_ID)
 
 
-def no_op_button(instance: Any, music_id: int):
+def no_op_button(unique_key: str, instance: Any, music_id: int):
     pass
 
-def add_to_playlist_button(instance: Any, music_id: int):
-    unique_button_key = f"add_music_{music_id}"
+def add_to_playlist_button(unique_key: str, instance: Any, music_id: int):
+    unique_button_key = f"{unique_key}_add_music_{music_id}"
+    print(f"totally unique: {unique_button_key}")
+
     if instance.button(key=unique_button_key, label="Adicionar à playlist"):
+        print("At least here?")
+
         response = do_add_music_to_playlist(
             st.session_state[SESSION_SHOULD_EXPLORE_PLAYLIST],
             music_id)
 
         if not response:
+            print("WTFFFFF")
             st.error("Erro ao adicionar música à playlist.")
         else:
+            print("HEEEEEY, YOU GOT HERE")
             st.session_state[SESSION_SHOULD_DISPLAY_MUSIC_ADDED] = True
             st.session_state[SESSION_CLEAR_SEARCH_RESULTS] = True
             st.rerun()
 
-def list_musics(musics: List[Dict[str, Any]], interaction_button: Callable[[Any, int], None] = no_op_button):
+def list_musics(unique_key: str, musics: List[Dict[str, Any]], interaction_button: Callable[[str, Any, int], None] = no_op_button):
     for music in musics:
         col1, col2 = st.columns((1,2.75))
         col1.image(music["image_url"], width=150)
@@ -33,9 +39,9 @@ def list_musics(musics: List[Dict[str, Any]], interaction_button: Callable[[Any,
         col2.write(music["artist"])
         col2.write(f"Gênero: {music['genre']}")
 
-        interaction_button(col2, music["id"])
+        interaction_button(unique_key, col2, music["id"])
 
-def music_search_box(unique_key: str, interaction_button: Callable[[Any, int], None] = no_op_button):
+def music_search_box(unique_key: str, interaction_button: Callable[[str, Any, int], None] = no_op_button):
     music_search_term = st.text_input(key=unique_key, label="Procure por uma música...")
     if music_search_term and not st.session_state[SESSION_CLEAR_SEARCH_RESULTS]:
         results = do_search(music_search_term)
@@ -43,10 +49,10 @@ def music_search_box(unique_key: str, interaction_button: Callable[[Any, int], N
             st.error("Nenhuma música encontrada")
         
         else:
-            list_musics(results, interaction_button)
+            list_musics(f"{unique_key}_regular", results, interaction_button)
     
     else:
         recommendations = do_get_music_recommendations_for_user(st.session_state[SESSION_USER_ID])
         if len(recommendations) > 0:
             st.markdown("**Recomendações:**")
-            list_musics(recommendations, interaction_button)
+            list_musics(f"{unique_key}_recommendations", recommendations, interaction_button)
