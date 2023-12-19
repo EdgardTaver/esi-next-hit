26616ce38ac19f58a18adb7fd8ac0d0c5a88d5c8
Edgard Taver <edgard.vtt@gmail.com>
2023-12-19 14:04:56 -0300

    organize imports

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-from app.infrastructure.database import create_music_table, create_playlists_table, create_playlist_music_table, create_users_table, start_users_database_connection
 from app.config import MUSICS_CSV_FILE
+from app.infrastructure.database import (create_music_table,
+                                         create_playlist_music_table,
+                                         create_playlists_table,
+                                         create_users_table,
+                                         start_users_database_connection)
 
 if __name__ == "__main__":
     connection = start_users_database_connection()
