72ba3154b4da337c0aafe7da9fe6233e08c679a7
Edgard Taver <edgard.vtt@gmail.com>
2023-12-19 14:04:26 -0300

    renames files to specific things

---
+++ app/frontend/main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+from app.frontend.session import initialize_session_variables
+
+from streamlit_option_menu import option_menu
+from app.frontend.session import initialize_session_variables
+from app.frontend.sections import explore_section, playlist_section, profile_section
+
+def frontend():
+    initialize_session_variables()
+
+    st.set_page_config(page_title="NextHit", page_icon=":musical_note:", layout="centered")
+
+    st.title(":musical_note: NextHit")
+    st.write("Encontre sua próxima música favorita")
+
+    selected = option_menu(
+        menu_title="Menu",
+        menu_icon="music-note-beamed",
+        options=["Descobrir", "Playlists", "Perfil"],
+        icons=["globe", "music-note-list", "person"],
+        orientation="horizontal",
+    )
+
+    if selected=="Descobrir":
+        explore_section()
+
+    if selected=="Playlists":
+        playlist_section()
+
+    if selected=="Perfil":
+        profile_section()
---
+++ frontend.py
@@ -0,0 +1,6 @@
+## python -m streamlit run app.py
+
+from app.frontend.main import frontend
+
+if __name__ == "__main__":
+    frontend()
